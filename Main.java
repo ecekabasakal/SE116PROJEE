dc5e23f00afe0aae7fb1df09d594338f0c28fe6e
Göksun Gürel <goksungurell@gmail.com>
2025-04-20 19:10:36 +0300

    added version info and current date-time display

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main{
-    public static public static void main(String[] args) {
-        System.out.println("hello world!");
+    public static void main(String[] args) {
+        
+        String versionNo = "1.0.0";
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String currentDateTime = LocalDateTime.now().format(formatter);
+
+        System.out.println("FSM DESIGNER " + versionNo);
+        System.out.println("Current date and time: " + currentDateTime);
+
     }
 }
