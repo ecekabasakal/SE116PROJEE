cbddfff6bdfc8376f15c5ba64459c22a01872470
ecekabasakal <ecekabasakal@hotmail.com>
2025-05-02 17:26:47 +0300

    exceptions added to LogManager

--- LogManager.java
+++ LogManager.java
@@ -6,53 +6,58 @@ public class LogManager {
     private BufferedWriter logWriter;
     private boolean isLogging = false;
 
-    
-    public void startLogging(String filename) {
+    public void startLogging(String filename) throws LogFileCreationException, LogAlreadyActiveException {
+        if (isLogging) {
+            throw new LogAlreadyActiveException();
+        }
+
         try {
-            if (isLogging) {
-                logWriter.close();  
-            }
-            logWriter = new BufferedWriter(new FileWriter(filename, false));  
+            logWriter = new BufferedWriter(new FileWriter(filename, false));
             isLogging = true;
             System.out.println("Started logging to: " + filename);
         } catch (IOException e) {
-            System.out.println("Error: Unable to start logging to " + filename);
+            throw new LogFileCreationException("Failed to create or open log file: " + filename);
         }
     }
-    public void stopLogging() {
+
+    public void stopLogging() throws LogFileWriteException {
         if (isLogging) {
             try {
                 logWriter.close();
                 isLogging = false;
                 System.out.println("STOPPED LOGGING");
             } catch (IOException e) {
-                System.out.println("Error: Unable to stop logging.");
+                throw new LogFileWriteException("Failed to close log file.");
             }
         } else {
             System.out.println("LOGGING was not enabled");
         }
     }
-    public void logCommand(String command) {
+
+    public void logCommand(String command) throws LogFileWriteException {
         if (isLogging) {
             try {
                 logWriter.write(command + "\n");
+                logWriter.flush(); // Ensure immediate write
             } catch (IOException e) {
-                System.out.println("Error: Unable to write to log file.");
+                throw new LogFileWriteException("Failed to write command to log file.");
             }
         }
     }
-    public void handleLogCommand(String command) {
+
+    public void handleLogCommand(String command) throws LogFileCreationException, LogAlreadyActiveException {
         String[] commandParts = command.split("\\s+");
         if (commandParts.length == 1) {
-            stopLogging();  
+            try {
+                stopLogging();
+            } catch (LogFileWriteException e) {
+                System.out.println(e.getMessage());
+            }
         } else if (commandParts.length == 2) {
             String filename = commandParts[1];
-            startLogging(filename);  
+            startLogging(filename);
         } else {
             System.out.println("Error: Invalid LOG command format.");
         }
     }
-
-
-
 }
