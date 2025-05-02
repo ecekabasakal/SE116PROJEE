31ffa93fb3042a68ec62c248ea04dfaed18b0053
ecekabasakal <ecekabasakal@hotmail.com>
2025-05-02 17:33:32 +0300

    exceptions added to SymbolManager

--- SymbolManager.java
+++ SymbolManager.java
@@ -4,29 +4,34 @@ import java.util.Set;
 public class SymbolManager {
     private Set<String> symbols = new HashSet<>();
 
-    public void handleSymbolsCommand(String command) {
-        String[] symbolArray = command.split("\\s+");
+    public void handleSymbolsCommand(String command) throws NonAlphanumericSymbolException, DuplicateSymbolWarningException, EmptySymbolListException {
+        try {
+            String[] symbolArray = command.split("\\s+");
 
-        if (symbolArray.length == 0) {
-            System.out.println("Current symbols: " + symbols);
-            return;
-        }
-
-        for (String symbol : symbolArray) {
-            if (!symbol.matches("[a-zA-Z0-9]+")) {
-                System.out.println("Warning: invalid symbol " + symbol);
-                continue;
+            if (symbolArray.length == 0) {
+                if (symbols.isEmpty()) {
+                    throw new EmptySymbolListException();
+                }
+                System.out.println("Current symbols: " + symbols);
+                return;
             }
-            
-        
-            String normalizedSymbol = symbol.toLowerCase();
 
-            if (symbols.contains(normalizedSymbol)) {
-                System.out.println("Warning: symbol " + symbol + " already declared.");
-            } else {
-                symbols.add(normalizedSymbol);  
-                System.out.println("Symbol " + symbol + " added.");
+            for (String symbol : symbolArray) {
+                if (!symbol.matches("[a-zA-Z0-9]+")) {
+                    throw new NonAlphanumericSymbolException("Warning: invalid symbol " + symbol);
+                }
+
+                String normalizedSymbol = symbol.toLowerCase();
+
+                if (symbols.contains(normalizedSymbol)) {
+                    throw new DuplicateSymbolWarningException("Warning: symbol " + symbol + " already declared.");
+                } else {
+                    symbols.add(normalizedSymbol);
+                    System.out.println("Symbol " + symbol + " added.");
+                }
             }
+        } catch (NonAlphanumericSymbolException | DuplicateSymbolWarningException | EmptySymbolListException e) {
+            throw e; 
         }
     }
 }
