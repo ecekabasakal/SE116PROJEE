44be61d8dbff47eb0ae0f19bcfd63c6618fb206c
ecekabasakal <ecekabasakal@hotmail.com>
2025-05-02 17:54:02 +0300

    exceptions are added to clear

--- Clear.java
+++ Clear.java
@@ -1,14 +1,22 @@
-public class Clear {
-    
-    private FSM fsm;
+public void clear() {
+    try {
+        // Semboller
+        if (symbols != null) {
+            symbols.clear();
+        }
 
-    public Clear(FSM fsm) {
-        this.fsm = fsm;
-    }
-    public void handleClear() {
-        fsm.clear();
+        // StatesManager varsa onu temizle
+        if (statesManager != null && statesManager.getStates() != null) {
+            statesManager.getStates().clear();
+        }
+
+        // TransitionManager varsa onu da temizle
+        if (transitionManager != null) {
+            transitionManager.clear();
+        }
+
         System.out.println("FSM cleared.");
+    } catch (Exception e) {
+        System.out.println("Error during FSM clearing: " + e.getMessage());
     }
 }
-
-
